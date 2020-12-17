b7012e5a1f0c362ba7907fb3120a8cac71e82b45
17538259840 <1260950572@qq.com>
2020-12-17 21:01:55 +0800

    第二次提交7

---
+++ myapplication3/src/main/java/com/example/myapplication3/utils/RetrofiUtils.java
@@ -0,0 +1,165 @@
+package com.example.myapplication3.utils;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+public class RetrofiUtils implements InetWorkInterface{
+    public static RetrofiUtils retrofiUtils;
+
+    private ApiServices apiServices;
+    private RetrofiUtils(){
+        OkHttpClient okHttpClient = new OkHttpClient().newBuilder().build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URLConstant.BASEURL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+
+        apiServices = retrofit.create(ApiServices.class);
+    }
+
+    public static RetrofiUtils getInstance(){
+        if (retrofiUtils ==null){
+            synchronized (RetrofiUtils.class){
+                if (retrofiUtils ==null){
+                    retrofiUtils = new RetrofiUtils();
+                }
+            }
+        }
+        return retrofiUtils;
+    }
+
+    @Override
+    public <T> void get(String url, INetCallBack<T> callBack) {
+        apiServices.get(url)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ResponseBody>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull ResponseBody responseBody) {
+                        try {
+                            String string = responseBody.string();
+                            //获取参数化类型
+                            Type[] genericInterfaces = callBack.getClass().getGenericInterfaces();
+                            //获取参数化类型的实际类型
+                            Type[] actualTypeArguments = ((ParameterizedType) genericInterfaces[0]).getActualTypeArguments();
+                            Type t = actualTypeArguments[0];
+                            Gson gson = new Gson();
+                            T result = gson.fromJson(string, t);
+                            callBack.success(result);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    @Override
+    public <T> void post(String url, INetCallBack<T> callBack) {
+        apiServices.post(url)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ResponseBody>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull ResponseBody responseBody) {
+                        try {
+                            String string = responseBody.string();
+                            //获取参数化类型
+                            Type[] genericInterfaces = callBack.getClass().getGenericInterfaces();
+                            //获取参数化类型的实际类型
+                            Type[] actualTypeArguments = ((ParameterizedType) genericInterfaces[0]).getActualTypeArguments();
+                            Type t = actualTypeArguments[0];
+                            Gson gson = new Gson();
+                            T result = gson.fromJson(string, t);
+                            callBack.success(result);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    @Override
+    public <T> void post(String url, HashMap<String, String> map, INetCallBack<T> callBack) {
+        apiServices.post(url,map)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ResponseBody>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull ResponseBody responseBody) {
+                        try {
+                            String string = responseBody.string();
+                            //获取参数化类型
+                            Type[] genericInterfaces = callBack.getClass().getGenericInterfaces();
+                            //获取参数化类型的实际类型
+                            Type[] actualTypeArguments = ((ParameterizedType) genericInterfaces[0]).getActualTypeArguments();
+                            Type t = actualTypeArguments[0];
+                            Gson gson = new Gson();
+                            T result = gson.fromJson(string, t);
+                            callBack.success(result);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+}
