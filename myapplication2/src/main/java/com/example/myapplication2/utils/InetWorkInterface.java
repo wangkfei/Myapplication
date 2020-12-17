03d42621fe40e6a1148decde6a9958028e569396
17538259840 <1260950572@qq.com>
2020-12-17 20:00:35 +0800

    第一次提交3

--- myapplication2/src/main/java/com/example/myapplication2/utils/InetWorkInterface.java
+++ myapplication2/src/main/java/com/example/myapplication2/utils/InetWorkInterface.java
@@ -1,4 +1,11 @@
 package com.example.myapplication2.utils;
 
+import java.util.HashMap;
+
 public interface InetWorkInterface {
+    
+    //不考虑回调的情况
+    public <T> void get(String url,INetCallBack<T> callBack);
+    public<T> void post(String url,INetCallBack<T> callBack);
+    public<T> void post(String url, HashMap<String,String> map,INetCallBack<T> callBack);
 }
