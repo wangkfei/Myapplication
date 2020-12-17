c9648dd0825986b89b4717894cc97cfa1ca64394
17538259840 <1260950572@qq.com>
2020-12-17 20:07:16 +0800

    第一次提交4

---
+++ myapplication2/src/main/java/com/example/myapplication2/utils/URLConstant.java
@@ -0,0 +1,7 @@
+package com.example.myapplication2.utils;
+
+public class URLConstant {
+    public static String BASEURL = "http://cdwan.cn:7000/";
+
+    public static String NEWLIST  = BASEURL+"exam2003/anewslist.json";
+}
